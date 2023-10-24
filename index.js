5624e425b2bdf4ee0ee0d68fa2b22ce42d167b5b
GtFoBAE05 <y.imannuel@gmail.com>
2023-10-24 18:29:10 +0700

    feat(fasilitas berbayar): add fasilitas berbayar UI and Integration to API

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const url = "http://localhost:3000/api/";
 
-module.exports = { url };
+function formattedDate(inputDate) {
+    const date = new Date(inputDate);
+
+    const day = date.getDate();
+    const month = date.toLocaleString("default", { month: "long" });
+    const year = date.getFullYear();
+    const hour = date.getHours();
+    const minutes = date.getMinutes();
+
+    return `${day < 10 ? "0" : ""}${day}-${month}-${year} ${hour}:${minutes}`;
+}
+
+module.exports = { url, formattedDate };

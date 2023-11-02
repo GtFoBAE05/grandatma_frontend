cec821041caaf5be8d31fd260ee4d402152cfaf7
GtFoBAE05 <y.imannuel@gmail.com>
2023-11-02 16:14:51 +0700

    refactor: add confirmation dialog and change alert to toast

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const url = "http://localhost:3000/api/";
+const url = "http://localhost:8080/api/";
 
 function formattedDate(inputDate) {
     const date = new Date(inputDate);
@@ -12,4 +12,110 @@ function formattedDate(inputDate) {
     return `${day < 10 ? "0" : ""}${day}-${month}-${year} ${hour}:${minutes}`;
 }
 
-module.exports = { url, formattedDate };
+function isTwoMonthsAhead(dateString) {
+    const inputDate = new Date(dateString);
+    const currentDate = new Date();
+    const twoMonthsLater = new Date();
+    twoMonthsLater.setMonth(currentDate.getMonth() + 2);
+
+    return inputDate > twoMonthsLater;
+}
+
+class ConfirmDialog {
+    constructor({ questionText, trueButtonText, falseButtonText, parent }) {
+        this.questionText = questionText || "Are you sure?";
+        this.trueButtonText = trueButtonText || "Yes";
+        this.falseButtonText = falseButtonText || "No";
+        this.parent = parent || document.body;
+
+        this.dialog = undefined;
+        this.trueButton = undefined;
+        this.falseButton = undefined;
+
+        this._createDialog();
+        this._appendDialog();
+    }
+
+    confirm() {
+        return new Promise((resolve, reject) => {
+            const somethingWentWrongUponCreation =
+                !this.dialog || !this.trueButton || !this.falseButton;
+            if (somethingWentWrongUponCreation) {
+                reject("Someting went wrong when creating the modal");
+                return;
+            }
+
+            this.dialog.showModal();
+            this.trueButton.focus();
+
+            this.trueButton.addEventListener("click", () => {
+                resolve(true);
+                this._destroy();
+            });
+
+            this.falseButton.addEventListener("click", () => {
+                resolve(false);
+                this._destroy();
+            });
+        });
+    }
+
+    _createDialog() {
+        this.dialog = document.createElement("dialog");
+        this.dialog.classList.add("confirm-dialog");
+
+        const question = document.createElement("div");
+        question.textContent = this.questionText;
+        question.classList.add("confirm-dialog-question");
+        this.dialog.appendChild(question);
+
+        const buttonGroup = document.createElement("div");
+        buttonGroup.classList.add("confirm-dialog-button-group");
+        this.dialog.appendChild(buttonGroup);
+
+        this.falseButton = document.createElement("button");
+        this.falseButton.classList.add(
+            "confirm-dialog-button",
+            "confirm-dialog-button--false"
+        );
+        this.falseButton.type = "button";
+        this.falseButton.textContent = this.falseButtonText;
+        buttonGroup.appendChild(this.falseButton);
+
+        this.trueButton = document.createElement("button");
+        this.trueButton.classList.add(
+            "confirm-dialog-button",
+            "confirm-dialog-button--true"
+        );
+        this.trueButton.type = "button";
+        this.trueButton.textContent = this.trueButtonText;
+        buttonGroup.appendChild(this.trueButton);
+    }
+
+    _appendDialog() {
+        this.parent.appendChild(this.dialog);
+    }
+
+    _destroy() {
+        this.parent.removeChild(this.dialog);
+        delete this;
+    }
+}
+
+class Toast {
+    constructor(message) {
+        this.message = message;
+        this.toast = document.createElement("div");
+        this.toast.id = "toast";
+        this.toast.innerHTML = `<p>${this.message}</p>`;
+        document.body.appendChild(this.toast);
+    }
+    show() {
+        this.toast.classList.add("show");
+        setTimeout(() => {
+            this.toast.classList.remove("show");
+        }, 3000); // Hide after 3 seconds
+    }
+}
+
+module.exports = { url, formattedDate, isTwoMonthsAhead };
